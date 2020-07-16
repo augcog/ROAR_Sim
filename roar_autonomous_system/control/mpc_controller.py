d1659041bce8e8ef444fe235a355cac3b05e97bd
Xiangyu Cai <xiangyu.cai@berkeley.edu>
2020-07-15 22:04:53 -0700

    add reference

--- roar_autonomous_system/control/mpc_controller.py
+++ roar_autonomous_system/control/mpc_controller.py
@@ -1,5 +1,11 @@
+# References:
+# https://github.com/asap-report/carla/blob/racetrack/PythonClient/racetrack/model_predictive_control.py
+
+""" This module contains MPC controller. """
+
 from roar_autonomous_system.control.controller import Controller
 from roar_autonomous_system.util.models import Control, Vehicle, Transform, Location
+
 
 class VehicleMPCController(Controller):
     def __init__(self, 
@@ -8,6 +14,8 @@ class VehicleMPCController(Controller):
                  steps_ahead=10,
                  dt=0.1):
         super.__init__(vehicle)
+
+
     
     def run_step(self, next_waypoint: Transform) -> Control:
         pass
