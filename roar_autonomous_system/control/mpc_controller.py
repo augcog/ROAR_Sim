3745ff78b6d8876bc5a524ab509e3cfbcf20160c
Xiangyu Cai <xiangyu.cai@berkeley.edu>
2020-07-15 21:36:00 -0700

    wip mpc controller

--- roar_autonomous_system/control/mpc_controller.py
+++ roar_autonomous_system/control/mpc_controller.py
@@ -2,4 +2,15 @@ from roar_autonomous_system.control.controller import Controller
 from roar_autonomous_system.util.models import Control, Vehicle, Transform, Location
 
 class VehicleMPCController(Controller):
-    def __init__(self, vehicle: Vehicle)
+    def __init__(self, 
+                 vehicle: Vehicle, 
+                 target_speed=float("inf"),
+                 steps_ahead=10,
+                 dt=0.1):
+        super.__init__(vehicle)
+    
+    def run_step(self, next_waypoint: Transform) -> Control:
+        pass
+
+    def sync(self):
+        pass
