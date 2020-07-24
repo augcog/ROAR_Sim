5551f1b6d3a3314e47563ed4af7e53173c7559db
Xiangyu Cai <xiangyu.cai@berkeley.edu>
2020-07-23 23:24:58 -0700

    add eq constraints

--- ROAR_simulation/roar_autonomous_system/control_module/mpc_controller.py
+++ ROAR_simulation/roar_autonomous_system/control_module/mpc_controller.py
@@ -18,3 +18,16 @@ from ROAR_simulation.roar_autonomous_system.control_module.controller import Con
 from ROAR_simulation.roar_autonomous_system.utilities_module.vehicle_models import VehicleControl, Vehicle
 from ROAR_simulation.roar_autonomous_system.utilities_module.data_structures_models import Transform, Location
 
+class _EqualityConstraints(object):
+    """Class for storing equality constraints in the MPC."""
+
+    def __init__(self, N, state_vars):
+        self.dict = {}
+        for symbol in state_vars:
+            self.dict[symbol] = N*[None]
+
+    def __getitem__(self, key):
+        return self.dict[key]
+
+    def __setitem__(self, key, value):
+        self.dict[key] = value
