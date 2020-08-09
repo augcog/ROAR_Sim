ddcbf909ce9422f64aa47165d365484c9512a473
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-08-09 16:51:33 -0700

    chritian please look

--- ROAR_simulation/roar_autonomous_system/perception_module/point_cloud_detector.py
+++ ROAR_simulation/roar_autonomous_system/perception_module/point_cloud_detector.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import time
 from typing import Optional
+from ROAR_simulation.roar_autonomous_system.utilities_module.data_structures_models import Transform, Location, Rotation
 
 
 class PointCloudDetector(Detector):
@@ -26,39 +27,29 @@ class PointCloudDetector(Detector):
         self.counter = 0
 
     def run_step(self) -> Optional[np.ndarray]:
-        points_3d = self.calculate_world_cords(max_points_to_convert=10000)
+        points_3d = self.calculate_world_cords(max_points_to_convert=20000)  # (Nx3)
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points_3d) #- np.mean(points_3d, axis=0))
+        pcd.estimate_normals()
 
-        print(np.amin(points_3d, axis=1), np.amax(points_3d, axis=1), self.agent.vehicle.transform.location)
-        self.pcd.points = o3d.utility.Vector3dVector(points_3d.T - np.mean(points_3d.T, axis=0))
-        # self.pcd.paint_uniform_color([0,0,0])
-        # points_3d = points_3d.T
-        # # filter out anything that is "above" my vehicle (so ground is definitely below my vehicle)
-        # # this part is shady, idk why it works
-        # ground_indicies = np.where(points_3d[:, 0] > self.agent.vehicle.transform.location.to_array()[0])
-        # ground_points = points_3d[ground_indicies]
-        # print(np.shape(ground_points.T))
-        # self.pcd.points = o3d.utility.Vector3dVector(ground_points.T)
-        #
-        # # # find the normals using Open3D
-        # self.pcd.points = o3d.utility.Vector3dVector(ground_points - np.mean(ground_points, axis=0))
-        # print(self.pcd.get_min_bound(), self.pcd.get_max_bound())
-        # self.pcd.estimate_normals(fast_normal_computation=True)
-        #
-        # # find normals that are less than the mean normal,
-        # # since I know that most of the things in front of me are going to be ground
-        # normals = np.asarray(self.pcd.normals)
-        # abs_diff = np.linalg.norm(normals - np.mean(normals, axis=0), axis=1)
-        # ground_loc = np.where(abs_diff < np.mean(abs_diff))
-        # ground = ground_points[ground_loc[0]]
-        #
-        # # turn it into Open3D PointCloud object again to utilize its remove outlier method
-        # # this is when I drive to the side, the opposing road will be recognized, but we don't want that
-        # self.pcd.points = o3d.utility.Vector3dVector(ground)
-        # new_pcd, indices = self.pcd.remove_radius_outlier(100, 2)
+        # since i know that the closest 200 points are always ground, I will use them as a reference for the
+        # normal vectors of the ground
+        pcd_tree = o3d.geometry.KDTreeFlann(pcd) # build KD tree for fast computation
+        [k, idx, _] = pcd_tree.search_knn_vector_3d(self.agent.vehicle.transform.location.to_array(), 200) # find points around me
+        points_near_me = np.asarray(pcd.points)[idx, :]  # 200 x 3
+        normals = np.asarray(pcd.normals)
+        u, s, vh = np.linalg.svd(points_near_me, full_matrices=False)  # use svd to find normals of points
+        avg_points_near_me_normal = vh[2, :]
+        abs_diff = np.linalg.norm(normals - avg_points_near_me_normal, axis=1)  # anything below avg is plane
+        planes = points_3d[abs_diff < np.mean(abs_diff)]
 
-        # project it back to Open3D PointCloud object for visualizations
-        # minus the mean for stationary visualization
-        # # self.pcd.points = o3d.utility.Vector3dVector(ground[indices])
+        # ground is anything below 1 m
+        ground = planes[planes[:, 2] < self.agent.vehicle.transform.location.z + 1]
+        pcd.points = o3d.utility.Vector3dVector(ground - np.mean(ground, axis=0))
+
+        print(np.amin(ground, axis=0), np.amax(ground, axis=0), self.agent.vehicle.transform.location, np.shape(ground))
+
+        self.pcd.points = pcd.points
         if self.counter == 0:
             self.vis.create_window(window_name="Open3d", width=400, height=400)
             self.vis.add_geometry(self.pcd)
@@ -73,39 +64,35 @@ class PointCloudDetector(Detector):
         self.counter += 1
         return points_3d
 
-    def calculate_world_cords(self, max_points_to_convert=5000):
+    def calculate_world_cords(self, max_points_to_convert=5000, max_detectable_distance=0.05):
         depth_img = self.agent.front_depth_camera.data.copy()
-        depth_img[depth_img > 0.05] = 0
-        depth_img = depth_img * 1000
-        # create a n x 2 array
-        image_size_y, image_size_x = np.shape(self.agent.front_depth_camera.data)
-        tx = image_size_x / 2
-        ty = image_size_y / 2
-        fx = fy = image_size_x / (2 * np.tan(self.agent.front_depth_camera.fov * np.pi / 360))
-
-        K = np.array([
-            [fx, 0, tx],
-            [0, fy, ty],
-            [0, 0, 1]
-        ])
-
         img_pos = np.indices((depth_img.shape[0], depth_img.shape[1])).transpose(1, 2, 0)
         img_pos = np.reshape(a=img_pos, newshape=(img_pos.shape[0] * img_pos.shape[1], 2))
         depth_array = np.reshape(a=depth_img, newshape=(depth_img.shape[0] * depth_img.shape[1], 1))
 
-        p2d = np.append(img_pos, np.ones(shape=(image_size_x * image_size_y, 1)), axis=1)
-        # print(np.shape(p2d))
-        # convert to raw_p2d
-        raw_p2d = np.linalg.inv(K) @ p2d.T
+        indicies_to_delete = np.where(depth_array > max_detectable_distance)[0]
+        depth_array = np.delete(depth_array, indicies_to_delete)
+        img_pos = np.delete(img_pos, indicies_to_delete, axis=0)
+        depth_array = depth_array * 1000
 
-        # convert to cords_xyz_1 -y,x-z
+        indices_to_select = np.random.choice(np.shape(img_pos)[0],
+                                             replace=False, size=min(np.shape(img_pos)[0], max_points_to_convert))
+
+        p2d = np.append(img_pos, np.ones(shape=(np.shape(img_pos)[0], 1)), axis=1)
+
+        p2d = np.take(p2d, indices_to_select, axis=0)
+        depth_array = np.take(depth_array, indices_to_select, axis=0)
+        # convert to raw_p2d
+        raw_p2d = np.linalg.inv(self.agent.front_depth_camera.intrinsics_matrix) @ p2d.T
+
+        # convert to cords_xyz_1
         cords_xyz_1 = np.vstack([
-            raw_p2d[0, :] * depth_array.T,
-            raw_p2d[1, :] * depth_array.T,
             raw_p2d[2, :] * depth_array.T,
+            raw_p2d[1, :] * depth_array.T,
+            -raw_p2d[0, :] * depth_array.T,
             np.ones((1, np.shape(raw_p2d)[1]))
         ])
 
-        points = self.agent.vehicle.transform.get_matrix() @ \
-                 self.agent.front_depth_camera.transform.get_matrix() @ cords_xyz_1
-        return points[:3, :]
+        points = self.agent.vehicle.transform.get_matrix() @ self.agent.front_depth_camera.transform.get_matrix() \
+                 @ cords_xyz_1
+        return points[:3, :].T
