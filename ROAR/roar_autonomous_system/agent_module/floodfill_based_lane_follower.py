247022b43dc5a7968eda68de5d756c3e59a3207f
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-09-30 01:22:35 -0700

    basic lane color following agent

--- ROAR/roar_autonomous_system/agent_module/floodfill_based_lane_follower.py
+++ ROAR/roar_autonomous_system/agent_module/floodfill_based_lane_follower.py
@@ -4,6 +4,8 @@ from ROAR.roar_autonomous_system.utilities_module.vehicle_models import Vehicle,
 from ROAR.roar_autonomous_system.control_module.pid_controller import VehiclePIDController
 from ROAR.roar_autonomous_system.perception_module.flood_fill_lane_detector import FloodfillLaneDetector
 from ROAR.roar_autonomous_system.control_module.pid_controller import PIDParam
+import cv2
+import numpy as np
 
 
 class FloodfillBasedLaneFollower(Agent):
@@ -15,5 +17,28 @@ class FloodfillBasedLaneFollower(Agent):
 
     def run_step(self, sensors_data: SensorsData, vehicle: Vehicle) -> VehicleControl:
         super().run_step(sensors_data=sensors_data, vehicle=vehicle)
-        self.floodfill_lane_detector.run_step()
-        return VehicleControl()
+        img = self.floodfill_lane_detector.run_step()
+
+        # left, front, right dot img location
+        left_dot_coord = (self.front_rgb_camera.image_size_x // 4, 400)
+        center_dot_coord = (self.front_rgb_camera.image_size_x // 2, 400)
+        right_dot_coord = (self.front_rgb_camera.image_size_x - (self.front_rgb_camera.image_size_x // 4), 400)
+        blue = [255,0,0]
+
+        left_ok = self._is_equal(img[left_dot_coord[::-1]], blue)
+        center_ok = self._is_equal(img[center_dot_coord[::-1]], blue)
+        right_ok = self._is_equal(img[right_dot_coord[::-1]], blue)
+
+        if center_ok:
+            return VehicleControl(throttle=0.5, steering=0)
+        elif left_ok:
+            return VehicleControl(throttle=0.3, steering=-0.5)
+        elif right_ok:
+            return VehicleControl(throttle=0.3, steering=0.5)
+        else:
+            return VehicleControl()
+
+    @staticmethod
+    def _is_equal(arr1, arr2):
+        # print(sum(arr1 == arr2))
+        return np.alltrue(arr1 == arr2)
--- runner_sim.py
+++ runner_sim.py
@@ -5,7 +5,8 @@ import os
 import warnings
 from ROAR.roar_autonomous_system.configurations.configuration import Configuration
 from ROAR.carla_client.carla_runner import CarlaRunner
-from ROAR.roar_autonomous_system.agent_module.json_waypoint_agent import JsonWaypointAgent
+# from ROAR.roar_autonomous_system.agent_module.json_waypoint_agent import JsonWaypointAgent
+from ROAR.roar_autonomous_system.agent_module.floodfill_based_lane_follower import FloodfillBasedLaneFollower
 from ROAR.roar_autonomous_system.agent_module.pure_pursuit_agent import PurePursuitAgent
 
 
@@ -19,8 +20,9 @@ def main():
                                npc_agent_class=PurePursuitAgent)
     try:
         my_vehicle = carla_runner.set_carla_world()
-        agent = JsonWaypointAgent(vehicle=my_vehicle, agent_settings=config.agent_config)
-        carla_runner.start_game_loop(agent=agent, use_manual_control=True)
+        # agent = JsonWaypointAgent(vehicle=my_vehicle, agent_settings=config.agent_config)
+        agent = FloodfillBasedLaneFollower(vehicle=my_vehicle, agent_settings=config.agent_config)
+        carla_runner.start_game_loop(agent=agent, use_manual_control=False)
     except Exception as e:
         logging.error(f"Something bad happened during initialization: {e}")
         carla_runner.on_finish()
