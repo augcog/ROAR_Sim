f99364ec0d154747c0941ca0bf11567dbbd335b5
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-09-30 01:24:52 -0700

    test

--- ROAR/roar_autonomous_system/agent_module/floodfill_based_lane_follower.py
+++ ROAR/roar_autonomous_system/agent_module/floodfill_based_lane_follower.py
@@ -17,27 +17,29 @@ class FloodfillBasedLaneFollower(Agent):
 
     def run_step(self, sensors_data: SensorsData, vehicle: Vehicle) -> VehicleControl:
         super().run_step(sensors_data=sensors_data, vehicle=vehicle)
-        img = self.floodfill_lane_detector.run_step()
+        try:
+            img = self.floodfill_lane_detector.run_step()
 
-        # left, front, right dot img location
-        left_dot_coord = (self.front_rgb_camera.image_size_x // 4, 400)
-        center_dot_coord = (self.front_rgb_camera.image_size_x // 2, 400)
-        right_dot_coord = (self.front_rgb_camera.image_size_x - (self.front_rgb_camera.image_size_x // 4), 400)
-        blue = [255,0,0]
+            # left, front, right dot img location
+            left_dot_coord = (self.front_rgb_camera.image_size_x // 4, 400)
+            center_dot_coord = (self.front_rgb_camera.image_size_x // 2, 400)
+            right_dot_coord = (self.front_rgb_camera.image_size_x - (self.front_rgb_camera.image_size_x // 4), 400)
+            blue = [255,0,0]
 
-        left_ok = self._is_equal(img[left_dot_coord[::-1]], blue)
-        center_ok = self._is_equal(img[center_dot_coord[::-1]], blue)
-        right_ok = self._is_equal(img[right_dot_coord[::-1]], blue)
+            left_ok = self._is_equal(img[left_dot_coord[::-1]], blue)
+            center_ok = self._is_equal(img[center_dot_coord[::-1]], blue)
+            right_ok = self._is_equal(img[right_dot_coord[::-1]], blue)
 
-        if center_ok:
-            return VehicleControl(throttle=0.5, steering=0)
-        elif left_ok:
-            return VehicleControl(throttle=0.3, steering=-0.5)
-        elif right_ok:
-            return VehicleControl(throttle=0.3, steering=0.5)
-        else:
+            if center_ok:
+                return VehicleControl(throttle=0.5, steering=0)
+            elif left_ok:
+                return VehicleControl(throttle=0.3, steering=-0.5)
+            elif right_ok:
+                return VehicleControl(throttle=0.3, steering=0.5)
+            else:
+                return VehicleControl()
+        except:
             return VehicleControl()
-
     @staticmethod
     def _is_equal(arr1, arr2):
         # print(sum(arr1 == arr2))
