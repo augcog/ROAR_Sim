34822339aef2ec767265ec7a7ff4508b1aebcdec
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-10-14 15:53:26 -0700

    default configuration fixed for carla

--- configurations/configuration.py
+++ configurations/configuration.py
@@ -56,15 +56,15 @@ class Configuration(BaseModel):
     print_keyboard_hint: bool = Field(default=False)
 
     synchronous_mode: bool = Field(default=False, title="Run Server and Client in Synchronos mode",
-        description="https://carla.readthedocs.io/en/0.9.9/adv_synchrony_timestep/#client-server-synchrony")
+                                   description="https://carla.readthedocs.io/en/0.9.9/adv_synchrony_timestep/#client-server-synchrony")
     no_rendering_mode: bool = Field(default=False,
                                     title="No Rendering at all, however you can enable save data to do a play back",
                                     description="https://carla.readthedocs.io/en/0.9.9/adv_rendering_options/")
     fixed_delta_seconds: float = Field(
-        default=1/20.0,
+        default=1 / 20.0,
         title="Fixed timestep with which server and client tick",
         description="Note that for us, this will ONLY take effect when you are on sync mode. "
                     "https://carla.readthedocs.io/en/0.9.9/adv_synchrony_timestep/#client-server-synchrony")
 
     should_spawn_npcs: bool = Field(default=False)
-    npc_config_file_path: str = Field(default="./data/npc_config.json")
+    npc_config_file_path: str = Field(default="./ROAR_Sim/configurations/npc_config.json")
